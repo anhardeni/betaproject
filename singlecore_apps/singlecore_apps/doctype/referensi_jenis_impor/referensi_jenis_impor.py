bf460409463d25081d7531338fa3786fa1784804
anhardeni <anhardeni@gmail.com>
2023-09-15 19:14:35 +0700

    add header link17

--- singlecore_apps/singlecore_apps/doctype/referensi_jenis_impor/referensi_jenis_impor.py
+++ singlecore_apps/singlecore_apps/doctype/referensi_jenis_impor/referensi_jenis_impor.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-class ReferensiJenisProsedur(Document):
+class ReferensiJenisImpor(Document):
 	pass
--- singlecore_apps/singlecore_apps/doctype/referensi_jenis_pib/referensi_jenis_pib.js
+++ singlecore_apps/singlecore_apps/doctype/referensi_jenis_pib/referensi_jenis_pib.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, AnharDeni and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Referensi Jenis Prosedur', {
+frappe.ui.form.on('Referensi Jenis Pib', {
 	// refresh: function(frm) {
 
 	// }
